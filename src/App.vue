ee84ba2d951eeb106501f6d7cb9e5e02758420cf
loloof64 <laurent.bernabe@gmail.com>
2021-05-08 17:10:42 +0200

    Added chess board.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainZone/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainZone from './components/MainZone.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainZone
   }
 }
 </script>
---
+++ src/components/MainZone.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button @click="newGame">New game</button>
+  </div>
+  <loloof64-chessboard
+    v-pre
+    ref="board"
+    size="300"
+    left="50"
+    top="50"
+    white_player_human="true"
+    black_player_human="true"
+  ></loloof64-chessboard>
+</template>
+
+<script>
+import "@loloof64/chessboard-component/dist";
+import { ref } from "vue";
+export default {
+  setup() {
+    const board = ref();
+
+    const newGame = function () {
+      board.value.newGame();
+    };
+
+    return {
+      board,
+      newGame,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
